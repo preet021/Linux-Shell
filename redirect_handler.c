f14aba7c24b0c51181c39c0432f32e41b4b09cac
Preet Thakkar <preet.thakkar@students.iiit.ac.in>
2018-09-14 12:43:45 +0530

    specification 2 done

--- redirect_handler.c
+++ redirect_handler.c
@@ -1,5 +1,10 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include "shell.h"
 
@@ -15,17 +20,17 @@ int redirect_handler (char* arg)
 	if (!flag)
 		return 0;
 	char* cmd = (char*)malloc(sizeof(char)*(arg_len+2));
+	cmd[0] = '\0';
 	int ind = 0, to_append = 0;
-	printf("arg: %s\n", arg);
 	while (arg[ind] != '<' && arg[ind] != '>' && ind < arg_len)
 	{
 		cmd[ind] = arg[ind];
 		ind++;
 	}
 	cmd[ind] = '\0';
-	printf("command: %s\n", cmd);
 	char* read_from = (char*)malloc(sizeof(char)*(arg_len+2));
 	char* write_to = (char*)malloc(sizeof(char)*(arg_len+2));
+	read_from[0] = write_to[0] = '\0';
 	for (int i=ind,j,k; i<arg_len;)
 	{
 		if (arg[i] == '>' && arg[i+1] != '>')
@@ -75,11 +80,65 @@ int redirect_handler (char* arg)
 	trim(s, read_from);
 	strcpy(s, write_to);
 	trim(s, write_to);
-	printf("read_from: %s\n", read_from);
-	printf("write_to: %s\n", write_to);
 	free(s);
-	free(cmd);
-	free(write_to);
-	free(read_from);
-	return 1;
+	int len_read_from = strlen(read_from), len_write_to = strlen(write_to);
+	if (len_write_to == 0 && len_read_from == 0)
+	{
+		fprintf(stderr, "Error: invalid format\n");
+		return 1;
+	}
+	int pid = fork(), status;
+	if (pid == 0)
+	{
+		struct stat st;
+		int read_fd = -1, write_fd = -1;
+		if (len_read_from > 0)
+		{
+			if (stat(read_from, &st) == -1)
+			{
+				fprintf(stderr, "Error: file \"%s\" does not exist\n", read_from);
+				return 1;	
+			}
+			if (!S_ISREG(st.st_mode))
+			{
+				fprintf(stderr, "Error: file to read from is not a regular file\n");
+				return 1;
+			}
+			read_fd = open(read_from, O_RDONLY);
+			if (read_fd == -1)
+			{
+				fprintf(stderr, "Error: failed to open file to read from\n");
+				return 1;
+			}
+			if (dup2(read_fd, 0) != 0)
+			{
+				fprintf(stderr, "Error: dup2 fail\n");
+				return 1;
+			}
+		}
+		if (len_write_to > 0)
+		{
+			if (to_append)
+				write_fd = open(write_to, O_WRONLY|O_CREAT|O_APPEND, 0644);
+			else write_fd = open(write_to, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+			if (dup2(write_fd, 1) != 1)
+			{
+				fprintf(stderr, "Error: dup2 fail\n");
+				return 1;
+			}
+		}
+		char** args = parseStr(cmd, " &\n\t\v\f\r\a");
+		int x = isAllowed(args[0]);
+		if (x >= 0)
+			executeCmd(args, allowed_execs[x]);
+		else execvp(args[0], args);
+		exit(0);
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+		free(cmd);
+		free(write_to);
+		free(read_from);
+	}
 }
